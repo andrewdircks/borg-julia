2726e707ff8a908832007cc063df2fdde8af9a1b
Andrew <andrewdircks@me.com>
2021-04-07 23:31:42 -0400

    initial commit, barebones implementation

---
+++ borg.jl
@@ -0,0 +1,136 @@
+using Libdl
+
+# TODO: make this variable
+const temp = "borg/libborg.so"
+
+mutable struct Configuration
+    so_dir::String
+    lib::Ptr{Nothing}
+end
+
+function Configure()::Configuration
+    so_dir = "borg/libborg.so"
+    lib = dlopen(so_dir)
+    return Configuration(so_dir, lib)
+end
+
+# configure and set finalizer
+config = Configure()
+finalizer(config) do x
+  dlclose(x.lib)
+end
+
+struct Borg
+    nVars::Int32
+    nObjs::Int32
+    nCons::Int32
+    bounds::Vector{Tuple{Float64, Float64}}
+    epsilons::Vector{Float64}
+    nfe::Int32
+    problem::Base.CFunction
+end
+
+struct Solution 
+    vars::Vector{Float64}
+    objs::Vector{Float64}
+    constrs::Vector{Float64}
+end
+
+function borg(nVars, nObjs, nCons, bounds, epsilons, nfe, julia_problem)::Borg
+    c_problem = @cfunction($julia_problem, Cvoid, (Ptr{Cdouble}, Ptr{Cdouble}, Ptr{Cdouble}))
+    return Borg(nVars, nObjs, nCons, bounds, epsilons, nfe, c_problem)
+end
+
+
+function dtlz2(vars, objs, constrs)::Cvoid 
+  nvars = 11
+  nobjs = 2
+  k = nvars - nobjs + 1
+
+  # load vars
+  vars = [unsafe_load(vars, i) for i = 1:nvars]
+
+  g = 0
+
+  for i = (nvars-k+1):nvars
+      g += (vars[i] - 0.5)^2
+  end
+
+  _objs = [1.0 + g, 1.0 + g]
+  
+  _objs[1] = _objs[1] * cos(0.5 * pi * vars[1])
+  _objs[2] = _objs[2] * sin(0.5 * pi * vars[1])
+
+  # store objs
+  for i = 1:nobjs
+      unsafe_store!(objs, _objs[i], i)
+  end
+
+  # appease function type definition
+  return Cvoid()
+end
+
+# set the bounds to this BORG_Problem reference
+function setBounds(bounds, nvars, ref)
+  for i = 0:(nvars-1)
+      ccall((:BORG_Problem_set_bounds, temp), Cvoid, (Ptr{Cvoid}, Int32, Cdouble, Cdouble), ref, i, bounds[i+1][1], bounds[i+1][2])
+  end
+end
+
+
+# set the epsilons to this BORG_Problem reference
+function setEpsilons(epsilons, nobjs, ref)
+  for i = 0:nobjs-1
+      ccall((:BORG_Problem_set_epsilon, temp), Cvoid, (Ptr{Cvoid}, Int32, Cdouble), ref, i, epsilons[i+1])
+  end
+end
+
+
+function getObjectives(sol, nObjs)
+  return [ccall((:BORG_Solution_get_objective), Cdouble, (Ptr{Cvoid}, Int32), sol, i) for i = 0:(nObjs-1)]
+end
+
+function getVariables(sol, nVars)
+  return [ccall((:BORG_Solution_get_variable), Cdouble, (Ptr{Cvoid}, Int32), sol, i) for i = 0:(nVars-1)]
+end
+
+function getConstraints(sol, nCons)
+  return [ccall((:BORG_Solution_get_constraint), Cdouble, (Ptr{Cvoid}, Int32), sol, i) for i = 0:(nCons-1)]
+end
+
+
+# process the result (pointer to archive)
+function process(result, borg) 
+    archive_size = ccall((:BORG_Archive_get_size, temp), Int32, (Ptr{Cvoid},), result)
+    solutions = Vector{Solution}(undef, archive_size)
+
+    for i = 0:(archive_size-1)
+      sol = ccall((:BORG_Archive_get, temp), Ptr{Cvoid}, (Ptr{Cvoid}, Int32), result, i)
+      vars = getVariables(sol, borg.nVars)
+      objs = getObjectives(sol, borg.nObjs)
+      constrs = getConstraints(sol, borg.nCons)
+      solutions[i+1] = Solution(vars, objs, constrs)
+  end
+
+  return solutions
+
+end
+
+function run(borg::Borg)
+    # create problem
+    ref = ccall((:BORG_Problem_create), Ptr{Cvoid}, (Int64, Int64, Int64, Ptr{Cvoid}), borg.nVars, borg.nObjs, borg.nCons, borg.problem)
+
+    # set bounds
+    setBounds(borg.bounds, borg.nVars, ref)
+
+    # set epsilons
+    setEpsilons(borg.epsilons, borg.nObjs, ref)
+
+    # run
+    result = ccall((:BORG_Algorithm_run, temp), Ptr{Cvoid}, (Ptr{Cvoid}, Int32), ref, borg.nfe)
+
+    # process results
+    solutions = process(result, borg)
+end
+
+
---
+++ test.jl
@@ -0,0 +1,37 @@
+include("borg.jl") 
+
+# problem specific globals
+nvars = 11
+nobjs = 2
+k = nvars - nobjs + 1
+
+function dtlz2(vars, objs, constrs)::Cvoid 
+    # load vars
+    vars = [unsafe_load(vars, i) for i = 1:nvars]
+
+    g = 0
+    # i = 2, 3, 4, 5, ... 10
+    # iJulia = 3, 4, 5, ... 11
+    for i = (nvars-k+1):nvars
+        g += (vars[i] - 0.5)^2
+    end
+
+    _objs = [1.0 + g, 1.0 + g]
+    
+    _objs[1] = _objs[1] * cos(0.5 * pi * vars[1])
+    _objs[2] = _objs[2] * sin(0.5 * pi * vars[1])
+
+    # store objs
+    for i = 1:nobjs
+        unsafe_store!(objs, _objs[i], i)
+    end
+  
+    # appease function type definition
+    return Cvoid()
+end
+
+bounds = repeat([(0.0, 1.0)], 11)
+epsilons = repeat([0.01], 2)
+test = borg(11, 2, 0, bounds, epsilons, 1000, dtlz2)
+result = run(test)
+print(result)
